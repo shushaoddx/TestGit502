f2dc7f21c332bcbd916bb0fd9970ed95db105454
ss <ss@example.com>
2020-07-31 16:11:59 +0800

    新建了一个项目TestGit502

---
+++ src/com/fs/Test.java
@@ -0,0 +1,14 @@
+package com.fs;
+
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author ss
+ * @Date 2020/7/31 16:09
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("我得github项目");
+    }
+}
