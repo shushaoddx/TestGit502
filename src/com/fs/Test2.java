d5721ed089300396671f584c05510480fefade16
ss <ss@example.com>
2020-07-31 16:19:06 +0800

    新建了Test2.java

---
+++ src/com/fs/Test2.java
@@ -0,0 +1,14 @@
+package com.fs;
+
+/**
+ * @ClassName Test2
+ * @Description TODO
+ * @Author ss
+ * @Date 2020/7/31 16:17
+ * @Version 1.0
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("xixi");
+    }
+}
